f7c909fcc46645a3e9b4c9ae9fb8af2b4ab7bb70
AhsanKhan7 <innocent.ahsan12345@gmail.com>
2020-11-29 23:11:59 -0800

    updating

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,22 @@
 import Header from "./components/Header";
 import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Footer from "./components/Footer";
+import HomeScreens from "./Screens/HomeScreens";
+import ProductScreen from "./Screens/ProductScreen";
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <h1>welcome to proshop</h1>
+          <Route path="/" component={HomeScreens} exact />
+          <Route path="/product/:id" component={ProductScreen} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 
