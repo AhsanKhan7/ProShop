141047b54a62baee2a48c0238a214103a9e8870d
AhsanKhan7 <innocent.ahsan12345@gmail.com>
2020-12-03 23:37:33 -0800

    cart reducer

--- frontend/src/reducers/cartReducer.js
+++ frontend/src/reducers/cartReducer.js
@@ -4,6 +4,7 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
   switch (action.type) {
     case CART_ADD_ITEM:
       const item = action.payload;
+
       const existItem = state.cartItems.find((x) => x.product === item.product);
 
       if (existItem) {
@@ -24,3 +25,30 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
       return state;
   }
 };
+
+// import { CART_ADD_ITEM } from "../constants/cartConstants";
+
+// export const cartReducer = (state = { cartItems: [] }, action) => {
+//   switch (action.type) {
+//     case CART_ADD_ITEM:
+//       const item = action.payload;
+//       const existItem = state.cartItems.find((x) => x.product === item.product);
+
+//       if (existItem) {
+//         return {
+//           ...state,
+//           cartItems: state.cartItems.map((x) =>
+//             x.product === existItem.product ? item : x
+//           ),
+//         };
+//       } else {
+//         return {
+//           ...state,
+//           cartItems: [...state.cartItems, item],
+//         };
+//       }
+
+//     default:
+//       return state;
+//   }
+// };
